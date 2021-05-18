0f326f4c6ef5e27e3b6858e1dde5193f4c7f5df0
adrianeto <77219420+adrianeto@users.noreply.github.com>
2021-05-18 16:26:09 -0500

    initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DemoApplication.class)
+class DemoApplicationTests {
+	@Autowired
+	private MockMvc mvc;
+
+
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/example/demo/MathPiTests.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MathPi.class)
+public class MathPiTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void renderPI() throws Exception {
+        this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793"));
+    }
+
+    @Test
+    void renderWrongPI() throws Exception {
+        this.mvc.perform(get("/Math/Pi").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isNotFound());
+    }
+}
